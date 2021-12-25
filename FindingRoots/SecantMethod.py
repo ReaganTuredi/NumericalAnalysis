9b177982e56b794a12afb8361c552d535ef6783f
Reagan Turedi <60759353+ReaganTuredi@users.noreply.github.com>
2021-12-24 20:44:25 -0500

    Update SecantMethod.py

--- FindingRoots/SecantMethod.py
+++ FindingRoots/SecantMethod.py
@@ -22,3 +22,37 @@ def secant_method(f,a,b,N):
             return 0
     temp_return=(a -(f(a)*(b - a))/(f(b) - f(a)))
     return temp_return
+#------------------------------------------------------
+#Error calculation 
+
+# error calculation for secant method
+def secant_method_error(f,a,b,N):
+    lists=[]
+    for i in range(1,N):
+        temp1 = a - f(a)*(b-a)/(f(b) - f(a))
+        temp2 = f(temp1)
+       
+        if f(a)*temp2 < 0:
+            a = a
+            b = temp1
+            lists.append(b)
+        elif f(b)*temp2 < 0:
+            a = temp1
+            b = b
+
+        elif temp2 == 0:
+            return temp1
+        else: 
+            return 0
+    temp_return=(a -(f(a)*(b - a))/(f(b) - f(a)))
+    return lists
+
+sme=secant_method_error(f,2,3,8)
+
+p_0_sm=[]
+p_1_sm=[]
+p_2_sm=[]
+for i in range(0,5):
+    p_0_sm.append((error(sme[i+1],sme[i],3.1618,0)))
+    p_1_sm.append((error(sme[i+1],sme[i],3.1618,1)))
+    p_2_sm.append((error(sme[i+1],sme[i],3.1618,2)))
